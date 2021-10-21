916f6176f2bfa949a41a08661cb36d9197449632
Dr Emacs <kv19@student.aau.dk>
2021-10-21 09:34:50 +0200

    ..

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,14 @@ from api.ImportApi import app
 from tests.json_test_data import *
 from unittest.mock import patch
 
+
 class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.client = TestClient(app)
 
-    @patch('file_io.FileWriter.add_to_queue')
+    @patch('file_io.FileWriter.FileWriter.add_to_queue')
     def test_correct_post(self, mock_queue):
         postItem = correctJson
         response = self.client.post("/uploadJsonDoc/", postItem)
@@ -18,7 +19,7 @@ class Test(unittest.TestCase):
         assert response.status_code == 200
         assert response.json() == "Json file successfully created"
 
-    @patch('file_io.FileWriter.add_to_queue')
+    @patch('file_io.FileWriter.FileWriter.add_to_queue')
     def test_incorrect_post(self, mock_queue):
         postItem = incorrectJson
         response = self.client.post("/uploadJsonDoc/", postItem)
