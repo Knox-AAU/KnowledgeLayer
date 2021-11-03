4e7c3bd4d36bd09fb02774913e759b63b3b86926
Oliver Harold <oliverhsj@hotmail.dk>
2021-11-03 10:58:30 +0100

    Update test.py

--- tests/performance_test/test.py
+++ tests/performance_test/test.py
@@ -7,8 +7,11 @@ import logging
 from publication_generator import PublicationGenerator
 from performace_test_suite import PerformanceTestSuite
 from doc_classification import PipelineManager
+from unittest.mock import patch
 
 logger = logging.getLogger()
+import data_access.WordCountDao
+
 
 def open_mock_endpoints():
     async def lemma_mock(request: Request):
@@ -21,16 +24,35 @@ def open_mock_endpoints():
     return threading.Thread(target=uvicorn.run, args=(app,), kwargs=dict(host="0.0.0.0", port=5000))
 
 
-def run_tests():
+def make_generator(paragraph_word_count):
     generator = PublicationGenerator("NJ")
     generator.repeat_amount = 20
     generator.stop_word_density = 0.5
+    generator.article_amount = 2
+    generator.paragraph_amount = 2
+    generator.paragraph_word_count = paragraph_word_count
+    return generator
+
+
+@patch('pre_processing.PreProcessor.lemmatize')
+@patch('data_access.WordCountDao.send_word_count')
+def run_tests(mock_lemma, mock_send_word):
     pipeline = PipelineManager()
-    suite = PerformanceTestSuite(pipeline.processStoredPublications, generator.publication_generator())
-    mock_endpoint_thread = open_mock_endpoints()
-    suite.setup_suite_func = lambda: mock_endpoint_thread.start()
-    with open(f'test_{datetime.datetime.now().date()}_2.txt', "a") as f:
-        f.write(str(suite.run()))
+    mock_lemma.side_effect = lambda x, y: x
+    mock_send_word.return_value = None
+    suite = PerformanceTestSuite(pipeline.processStoredPublications, make_generator(10).publication_generator())
+
+    # with open(f'test_{datetime.datetime.now().date()}_2.txt', "a") as f:
+    #     f.write(str(suite.run()))
+    for i in range(10):
+        generator = make_generator(i * 100)
+        suite.data_generator = generator.publication_generator()
+        print(str(suite.run()))
+    #
+    # wordcount = generator.article_amount * generator.paragraph_amount * generator.paragraph_word_count
+    # data = suite.run()
+    # import numpy as np
+    # print(((len(data) - 1) * wordcount) / np.array(data[1:]).sum())
 
 
 if __name__ == "__main__":
