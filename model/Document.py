3025e1bebd2b1af9b10378a6da5d717ff71d09ae
janushannesarson <45720841+janushannesarson@users.noreply.github.com>
2021-11-04 13:23:03 +0100

    added documentation to the models

--- model/Document.py
+++ model/Document.py
@@ -3,9 +3,16 @@ from typing import List
 
 class Article:
     """
-
+    An article is a general term and is used to either express a Grundfos manual or a Nordjyske article for example
     """
+
     def __init__(self, title, body, path):
+        """
+
+        :param title: Title of the article
+        :param body: Text of the article
+        :param path: The filepath to where the original file is located
+        """
         self.title = title
         self.body = body
         self.path = path
@@ -13,8 +20,12 @@ class Article:
 
 class Document:
     """
-
+    A general class intended to be compatiable with all data sources including data from Grundfos and Nordjyske
     """
+
     def __init__(self, publisher: str):
+        """
+        :param publisher: Examples: Grundfos, Nordjyske
+        """
         self.publisher = publisher
         self.articles: List[Article] = []
--- model/Word.py
+++ model/Word.py
@@ -3,5 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Word:
+    """
+    Contains a word and the amount of appearances
+    """
     word: str
     amount: str
