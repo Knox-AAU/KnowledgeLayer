83baac28fb6463177aff1385ae5f63fcacbb7d34
janushannesarson <45720841+janushannesarson@users.noreply.github.com>
2021-11-04 13:40:43 +0100

    added documentation to WordCountDao.py

--- data_access/data_transfer_objects/DocumentWordCountDto.py
+++ data_access/data_transfer_objects/DocumentWordCountDto.py
@@ -6,7 +6,8 @@ from model.Word import Word
 @dataclass
 class DocumentWordCountDto:
     """
-
+    Data transfer object for the word count result of a document. Adheres to the /WordCount endpoint JSON schema. See
+    the Knox wiki page.
     """
     articletitle: str
     filepath: str
