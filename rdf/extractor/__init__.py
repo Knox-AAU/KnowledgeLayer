a87920aec7201101e29f1128b864f8f2e7d24b74
Xamalf <casperbis1999@gmail.com>
2021-11-23 11:04:16 +0100

    Added new classes to init for triple extraction

--- rdf/extractor/__init__.py
+++ rdf/extractor/__init__.py
@@ -1,3 +1,5 @@
 from .TripleExtractorEnum import TripleExtractorEnum
 from .TripleExtractor import TripleExtractor
 from .NJTripleExtractor import NJTripleExtractor
+from .GFTripleExtractor import GFTripleExtractor
+from .TripleExtractor import TripleExtractor
