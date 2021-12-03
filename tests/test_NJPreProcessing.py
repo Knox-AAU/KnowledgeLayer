f43dd019f5ecfc7e9031f37c51a7add300a77eba
Oliver Harold <oliverhsj@hotmail.dk>
2021-12-03 10:59:19 +0100

    Revert "Testing GithubAction"
    This reverts commit e38d1916b60b39bfd750a271292d5ef9e232baf0.

---
+++ tests/test_NJPreProcessing.py
@@ -0,0 +1,96 @@
+from typing import List
+import pytest
+from unittest.mock import patch
+import unittest
+import requests.exceptions
+
+import exceptions
+from model.Document import Document, Article
+from pre_processing import NJPreProcessor
+
+xfail = pytest.mark.xfail
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.preproc = NJPreProcessor()
+
+    def create_article(self, title: str, body: str) -> Article:
+        return Article(title, body, "/testpath.extension")
+
+    def setup_data(self, articleBodies: List[str]) -> Document:
+        makeArticle = lambda title, body: Article(title, body, "/testpath.extension")
+        document = Document("Test Publisher")
+        document.articles = [makeArticle(f'TestTitle Nr. {i}', v) for i, v in enumerate(articleBodies)]
+        return document
+
+    @patch('pre_processing.PreProcessor.lemmatize')
+    def test__process__removes_stopwords(self, mock_post):
+        # Arrange
+        data = self.setup_data(["hej, jeg vil gerne. testes..,"])
+        mock_post.side_effect = lambda x, y: x
+
+        # Act
+        output = self.preproc.process(data).articles[0].body
+
+        # Assert
+        self.assertEqual("hej gerne testes", output)
+
+    @patch('pre_processing.PreProcessor.lemmatize')
+    def test__process__removes_stopwords_multiple(self, mock_post):
+        # Arrange
+        data = self.setup_data(["hej, jeg vil gerne. testes..,", "hej, jeg vil gerne. testes.., I. DAG. TAK."])
+        mock_post.side_effect = lambda x, y: x
+
+        # Act
+        output = [p.body for p in self.preproc.process(data).articles]
+
+        # Assert
+        self.assertEqual(["hej gerne testes", "hej gerne testes I dag tak"], output)
+
+
+    @patch('pre_processing.PreProcessor.lemmatize')
+    def test__process__convert_to_modern_danish_works(self, mock_post):
+        # Arrange
+        data = self.setup_data(["hej, jeg vil gerne. testes..,", "hej, Rumskibet Karsten vil. testes.., I. DAG. TAK."])
+        mock_post.side_effect = lambda x, y: x
+
+        # Act
+        output = [p.body for p in self.preproc.process(data).articles]
+
+        # Assert
+        self.assertEqual(["hej gerne testes", "hej rumskibet Karsten testes I dag tak"], output)
+
+    @patch('pre_processing.PreProcessor.lemmatize')
+    def test__process__lemmatize(self, mock_post):
+        # Arrange
+        data = self.setup_data(["tanken er fyldt op"])
+        mock_post.return_value = "tanken fyldet op"
+
+        # Act
+        output = self.preproc.process(data).articles[0].body
+
+        # Assert
+        self.assertEqual("tanken fyldet op", output)
+
+    @patch('requests.post')
+    def test__process__lemmatize_raise_unparseable(self, mock_post):
+        # Arrange
+        data = self.setup_data(["tanken er fyldt op"])
+        mock_post.side_effect = lambda x, y: (_ for _ in ()).throw(requests.exceptions.InvalidSchema())
+
+        # Act & Assert
+        with self.assertRaises(exceptions.UnparsableException) as ctx:
+            self.preproc.process(data)
+
+    # @patch('requests.post')
+    # def test__process__lemmatize_raise_postFailed(self, mock_post):
+    #     # Arrange
+    #     data = self.setup_data(["tanken er fyldt op"])
+    #     mock_post.side_effect = lambda x, y: (_ for _ in ()).throw(requests.exceptions.ConnectionError())
+    #
+    #     # Act & Assert
+    #     with self.assertRaises(exceptions.PostFailedException):
+    #         self.preproc.process(data)
