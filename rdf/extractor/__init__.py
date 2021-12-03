926ef7198fa76165310a1070f93e210a742e1fb7
Oliver Harold <oliverhsj@hotmail.dk>
2021-12-03 13:58:41 +0100

    Update __init__.py

--- rdf/extractor/__init__.py
+++ rdf/extractor/__init__.py
@@ -1,3 +1,4 @@
 from .TripleExtractorEnum import TripleExtractorEnum
 from .TripleExtractor import TripleExtractor
 from .NJTripleExtractor import NJTripleExtractor
+from .GFTripleExtractor import GFTripleExtractor
