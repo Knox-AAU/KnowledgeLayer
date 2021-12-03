df47bfafdb58edf152e7d4b208849442f1714542
Oliver Harold <oliverhsj@hotmail.dk>
2021-12-03 13:53:54 +0100

    Revert "Added new classes to init for triple extraction"
    This reverts commit a87920aec7201101e29f1128b864f8f2e7d24b74.

--- rdf/extractor/__init__.py
+++ rdf/extractor/__init__.py
@@ -1,5 +1,3 @@
 from .TripleExtractorEnum import TripleExtractorEnum
 from .TripleExtractor import TripleExtractor
 from .NJTripleExtractor import NJTripleExtractor
-from .GFTripleExtractor import GFTripleExtractor
-from .TripleExtractor import TripleExtractor
